045374ef0b84f3eb850f488eeebb20bea14ad1b2
Siddharth Trivedi <strivedi_be20@thapar.edu>
2022-02-05 13:38:07 +0530

    ENJOY

--- LABASS1/6.py
+++ LABASS1/6.py
@@ -1,8 +1,8 @@
-b=int(input("Enter basic pay:"))
+bas=int(input("Enter basic pay:"))
 print("Salary: ")
-if b<=10000:
-    print(b+0.2*b+0.8*b)
-elif  b<=20000 and b>=10000:
-    print(b+0.25*b+0.9*b)
+if bas<=10000:
+    print(bas+0.2*bas+0.8*bas)
+elif  bas<=20000 and bas>=10000:
+    print(bas+0.25*bas+0.9*bas)
 else:
-    print(b+0,3*b+0.95*b)
+    print(bas+0.3*bas+0.95*bas)
