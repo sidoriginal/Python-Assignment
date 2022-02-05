c31ee934082beb17d68b5632b421e0c00c06ecf0
Siddharth Trivedi <strivedi_be20@thapar.edu>
2022-02-05 12:50:07 +0530

    ENJOY

--- LABASS1/1.py
+++ LABASS1/1.py
@@ -1,10 +1,9 @@
 import math
 x=int(input("Enter x"))
 n=int(input("Enter n"))
-s=1;
+s=1
 for i in range(1,n+1):
-    j=math.pow(x,i)/math.factorial(i);
-    print(j);
-    s=s+j;
-print(s);
+    j=math.pow(x,i)/math.factorial(i)
+    s=s+j
+print(s)
 
